346c25d4cf1cabe54772f2b1990b5ef8f4529621
wastydev <79198623+wastydev@users.noreply.github.com>
2024-12-05 23:02:03 +0300

    Update stat.js

--- models/stat.js
+++ models/stat.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose');
 
 const statSchema = new mongoose.Schema({
-  userId: { type: String, required: true, unique: true },
-  messages: { type: Number, default: 0 }, // Kullanıcının mesaj sayısı
-  voiceTime: { type: Number, default: 0 }, // Ses kanalında geçirilen süre (saniye cinsinden)
-  lastVoiceEnterTime: { type: Date, default: null }, // Ses kanalına giriş zamanı
+  userId: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  }, // Kullanıcı ID'si, her kullanıcı için benzersiz olmalı
+  messages: { 
+    type: Number, 
+    default: 0 
+  }, // Kullanıcının gönderdiği toplam mesaj sayısı
+  voiceTime: { 
+    type: Number, 
+    default: 0 
+  }, // Kullanıcının toplam ses süresi (saniye cinsinden)
+  lastVoiceEnterTime: { 
+    type: Date, 
+    default: null 
+  } // Kullanıcının ses kanalına en son giriş yaptığı zaman
 });
 
 module.exports = mongoose.model('Stat', statSchema);
